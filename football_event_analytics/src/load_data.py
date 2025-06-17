9983fbcae27b586dc7f607c6f7f991687c415628
egechn <49732368+egechn@users.noreply.github.com>
2025-06-17 18:06:46 +0200

    Delete football_event_analytics directory

--- football_event_analytics/src/load_data.py
+++
@@ -1,7 +0,0 @@
-import json
-import pandas as pd
-
-def load_event_data(path):
-    with open(path, 'r') as f:
-        data = json.load(f)
-    return pd.json_normalize(data['events'])
--- football_event_analytics/src/pressing.py
+++
@@ -1,4 +0,0 @@
-def calculate_pressing_intensity(events_df):
-    pressure_events = events_df[events_df['type.name'] == 'Pressure']
-    pressures_per_90 = len(pressure_events) / (events_df['minute'].max() / 90)
-    return pressures_per_90
